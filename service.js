055a7c171f0ac5ffdcb72dac661ee114e9dd8021
Eziohao <eziohao@gmail.com>
2016-04-08 12:55:07 -0400

    change XML

--- service.js
+++ service.js
@@ -1,7 +1,15 @@
 var p=require('xml2js');
 var fs=require('fs');
 
+var operations=fs.readFileSync('operations.json');
+
 fs.readFile('client.xml', function(err,data){
 	if(err) throw err;
-	console.log(p.parseString(data.toString()));
+	p.parseString(data, function(err,result){
+		if (err) {
+			throw err;// statement
+		}
+		console.dir(result.conversation.conversationInteractions);
+
+	})
 });
